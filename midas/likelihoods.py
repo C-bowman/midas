17a08d491b098d5f8233c9b0cc7c379e0e8ac0e8
C-bowman <chris.bowman.physics@gmail.com>
2025-05-28 15:36:10 +0100

    work on input validation of likelihood functions

--- midas/likelihoods.py
+++ midas/likelihoods.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from numpy import ndarray, log, exp, logaddexp, sqrt, pi, zeros
+from numpy import ndarray, log, exp, logaddexp, sqrt, pi, zeros, isfinite
 from midas.state import PlasmaState
 from midas.models import DiagnosticModel
 
@@ -31,7 +31,13 @@ class DiagnosticLikelihood:
         A name or other identifier for the diagnostic as a string.
 
     """
-    def __init__(self, diagnostic_model: DiagnosticModel, likelihood: LikelihoodFunction, name: str):
+
+    def __init__(
+        self,
+        diagnostic_model: DiagnosticModel,
+        likelihood: LikelihoodFunction,
+        name: str,
+    ):
         self.forward_model = diagnostic_model
         self.likelihood = likelihood
         self.name = name
@@ -68,7 +74,9 @@ class DiagnosticLikelihood:
         for field_param in field_jacobians.keys():
             field_name = PlasmaState.field_parameter_map[field_param]
             slc = PlasmaState.slices[field_param]
-            grad[slc] = (dL_dp @ model_jacobians[field_name]) @ field_jacobians[field_param]
+            grad[slc] = (dL_dp @ model_jacobians[field_name]) @ field_jacobians[
+                field_param
+            ]
 
         return grad
 
@@ -91,21 +99,22 @@ class GaussianLikelihood(LikelihoodFunction):
         The standard deviations corresponding to each element in ``y_data`` as a 1D array.
     """
 
-    def __init__(
-        self,
-        y_data: ndarray,
-        sigma: ndarray,
-    ):
+    def __init__(self, y_data: ndarray, sigma: ndarray):
         self.y = y_data
         self.sigma = sigma
+
+        validate_likelihood_data(
+            values=y_data, uncertainties=sigma, likelihood_name=self.__class__.__name__
+        )
+
         self.n_data = self.y.size
         self.inv_sigma = 1.0 / self.sigma
-        self.inv_sigma_sqr = self.inv_sigma ** 2
+        self.inv_sigma_sqr = self.inv_sigma**2
         self.normalisation = -log(self.sigma).sum() - 0.5 * log(2 * pi) * self.n_data
 
     def log_likelihood(self, predictions: ndarray) -> float:
         z = (self.y - predictions) * self.inv_sigma
-        return -0.5 * (z ** 2).sum() + self.normalisation
+        return -0.5 * (z**2).sum() + self.normalisation
 
     def predictions_derivative(self, predictions: ndarray) -> ndarray:
         return (self.y - predictions) * self.inv_sigma_sqr
@@ -121,13 +130,14 @@ class LogisticLikelihood(LikelihoodFunction):
     :param sigma: \
         The uncertainties corresponding to each element in ``y_data`` as a 1D array.
     """
-    def __init__(
-        self,
-        y_data: ndarray,
-        sigma: ndarray,
-    ):
+
+    def __init__(self, y_data: ndarray, sigma: ndarray):
         self.y = y_data
         self.sigma = sigma
+
+        validate_likelihood_data(
+            values=y_data, uncertainties=sigma, likelihood_name=self.__class__.__name__
+        )
 
         # pre-calculate some quantities as an optimisation
         self.n_data = self.y.size
@@ -144,6 +154,47 @@ class LogisticLikelihood(LikelihoodFunction):
         return (2 / (1 + exp(-z)) - 1) * self.inv_scale
 
 
+def validate_likelihood_data(
+    values: ndarray, uncertainties: ndarray, likelihood_name: str
+):
+    valid_types = isinstance(values, ndarray) and isinstance(uncertainties, ndarray)
+    if not valid_types:
+        raise TypeError(
+            f"""\n
+            \r[ {likelihood_name} error ]
+            \r>> The data values and uncertainties must be an instance of
+            \r>> (or inherit from) numpy.ndarray. Instead, the given types were
+            \r>> {type(values)}
+            \r>> and
+            \r>> {type(uncertainties)}
+            """
+        )
 
-def validate_likelihood_data(values: ndarray, uncertainties: ndarray, error_source: str):
- pass
+    valid_shapes = (
+        values.ndim == 1
+        and uncertainties.ndim == 1
+        and values.size == uncertainties.size
+    )
+    if not valid_shapes:
+        raise ValueError(
+            f"""\n
+            \r[ {likelihood_name} error ]
+            \r>> The data values and uncertainties arrays must be one-dimensional
+            \r>> and of equal size, but instead have shapes
+            \r>> {values.shape} and {uncertainties.shape}.
+            """
+        )
+
+    valid_values = (
+        isfinite(values).all()
+        and isfinite(uncertainties).all()
+        and (uncertainties > 0.0).all()
+    )
+    if not valid_values:
+        raise ValueError(
+            f"""\n
+            \r[ {likelihood_name} error ]
+            \r>> The data values and uncertainties arrays must contain only finite
+            \r>> values, and all uncertainties must have values greater than zero.
+            """
+        )
