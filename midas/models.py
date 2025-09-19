b572898ea8604955ac92d7e3296351070611fc2e
C-bowman <chris.bowman.physics@gmail.com>
2025-09-19 17:07:12 +0100

    Added a prototype model for linear diagnostics

--- midas/models.py
+++ midas/models.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from numpy import ndarray
 from midas.parameters import FieldRequest, ParameterVector
 
 
@@ -13,3 +14,18 @@ class DiagnosticModel(ABC):
     @abstractmethod
     def predictions_and_jacobians(self, **kwargs):
         pass
+
+
+class LinearDiagnosticModel(DiagnosticModel):
+    def __init__(self, field: FieldRequest, model_matrix: ndarray):
+        self.parameters = []
+        self.field_requests = [field]
+        self.field_name = field.name
+        self.A = model_matrix
+        self.jacobian = {self.field_name: self.A}
+
+    def predictions(self, **kwargs):
+        return self.A @ kwargs[self.field_name]
+
+    def predictions_and_jacobians(self, **kwargs):
+        return self.A @ kwargs[self.field_name], self.jacobian
