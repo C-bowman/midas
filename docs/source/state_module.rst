cd03963ab29ab5518196d40ec5b9ac91d86cd1cb
C-bowman <chris.bowman.physics@gmail.com>
2025-12-07 17:30:37 +0000

    Added a new `PlasmaState.build_bounds` method, including docs and a unit test

--- docs/source/state_module.rst
+++ docs/source/state_module.rst
@@ -4,4 +4,4 @@ The ``PlasmaState`` class builds and manages the parametrisation of the posterio
 distribution, and provides functions to manipulate parameters and posterior samples.
 
 .. autoclass:: midas.PlasmaState
-   :members: build_posterior, split_parameters, merge_parameters, split_samples
+   :members: build_posterior, split_parameters, merge_parameters, split_samples, build_bounds
--- tests/utilities.py
+++ tests/utilities.py
@@ -1,4 +1,4 @@
-from numpy import ndarray, ones_like, linspace, zeros
+from numpy import ndarray, ones_like, linspace, zeros, ones
 from numpy.random import default_rng
 from midas import Parameters, Fields
 from midas.models import DiagnosticModel
@@ -29,3 +29,24 @@ class StraightLine(DiagnosticModel):
         sigma = zeros(x.size) + 2.0
         y = 3.5 * x - 2.0 + rng.normal(size=x.size, scale=sigma)
         return x, y, sigma
+
+
+
+class Polynomial(DiagnosticModel):
+    def __init__(self, x_axis: ndarray, order: int = 2):
+        self.x = x_axis
+        self.parameters = Parameters(
+            ("poly_coefficients", 1 + order),
+        )
+        self.fields = Fields()
+        self.jacobian = ones([self.x.size, 1 + order])
+        for i in range(1, order + 1):
+            self.jacobian[:, i] = self.jacobian[:, i - 1] * self.x
+
+    def predictions(self, gradient: float, y_intercept: float) -> ndarray:
+        return self.jacobian @ self.x
+
+    def predictions_and_jacobians(self, gradient: float, y_intercept: float) -> tuple:
+        predictions = self.jacobian @ self.x
+        jacobians = {"poly_coefficients": self.jacobian}
+        return predictions, jacobians
