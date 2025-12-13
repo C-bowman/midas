51b7640cbc159ec214073e41395d9bf59829118d
C-bowman <chris.bowman.physics@gmail.com>
2025-12-13 22:56:50 +0000

    Added tests for applying uncertainty models to likelihood functions

--- tests/test_likelihoods.py
+++ tests/test_likelihoods.py
@@ -3,7 +3,7 @@ from numpy import array, nan
 from scipy.optimize import minimize, approx_fprime
 
 from midas.likelihoods import GaussianLikelihood, LogisticLikelihood, CauchyLikelihood
-from midas.likelihoods import ScaledGaussianLikelihood
+from midas.likelihoods import ConstantUncertainty, LinearUncertainty
 from midas.likelihoods import DiagnosticLikelihood
 from midas import posterior, PlasmaState
 
@@ -15,8 +15,8 @@ from utilities import StraightLine
     [GaussianLikelihood, LogisticLikelihood, CauchyLikelihood],
 )
 def test_likelihood_validation(likelihood):
-    y = array([1., 3., 4.])
-    sig = array([5., 5., 3.])
+    y = array([1.0, 3.0, 4.0])
+    sig = array([5.0, 5.0, 3.0])
 
     # check the type validation
     with pytest.raises(TypeError):
@@ -41,8 +41,8 @@ def test_likelihood_validation(likelihood):
 )
 def test_likelihoods_predictions_gradient(likelihood):
     test_values = array([3.58, 2.11, 7.89])
-    y = array([1., 3., 4.])
-    sig = array([5., 5., 3.])
+    y = array([1.0, 3.0, 4.0])
+    sig = array([5.0, 5.0, 3.0])
     func = likelihood(y, sig)
 
     analytic_grad, _ = func.derivatives(predictions=test_values)
@@ -51,46 +51,56 @@ def test_likelihoods_predictions_gradient(likelihood):
     assert max_abs_err < 1e-6
 
 
-def test_scaled_gaussian():
+@pytest.mark.parametrize(
+    "likelihood_function",
+    [GaussianLikelihood, LogisticLikelihood, CauchyLikelihood],
+)
+def test_parameterised_uncertainties(likelihood_function):
     x, y, sigma = StraightLine.testing_data()
-    likelihood_func = ScaledGaussianLikelihood(
-        y_data=y,
-        sigma=sigma,
-        scale_parameter_name="error_scale"
+
+    def run_uncertainty_model(uncertainty_model):
+        likelihood_func = likelihood_function(
+            y, uncertainty_model,
+        )
+
+        model = StraightLine(x_axis=x)
+
+        line_likelihood = DiagnosticLikelihood(
+            likelihood=likelihood_func, diagnostic_model=model, name="straight_line"
+        )
+
+        PlasmaState.build_posterior(
+            diagnostics=[line_likelihood], priors=[], field_models=[]
+        )
+
+        test_params = {
+            "gradient": 1.0,
+            "y_intercept": -1.0,
+            "constant_error": 0.5,
+            "test_constant_error": 0.3,
+            "test_fractional_error": 0.05,
+        }
+        test_point = PlasmaState.merge_parameters(test_params)
+
+        opt_result = minimize(
+            fun=posterior.cost, x0=test_point, jac=posterior.cost_gradient
+        )
+
+        num_grad = approx_fprime(
+            xk=test_point,
+            f=posterior.log_probability,
+            epsilon=1e-8,
+        )
+        analytic_grad = posterior.gradient(test_point)
+
+        assert abs(analytic_grad / num_grad - 1).max() < 1e-5
+
+    constant_uncertainty = ConstantUncertainty(
+        n_data=y.size, parameter_name="constant_error"
     )
 
-    model = StraightLine(x_axis=x)
+    run_uncertainty_model(constant_uncertainty)
 
-    line_likelihood = DiagnosticLikelihood(
-        likelihood=likelihood_func,
-        diagnostic_model=model,
-        name="straight_line"
-    )
+    linear_uncertainty = LinearUncertainty(y_data=y, parameter_prefix="test")
 
-    PlasmaState.build_posterior(
-        diagnostics=[line_likelihood],
-        priors=[],
-        field_models=[]
-    )
-
-    test_params = {
-        "gradient": 1.0,
-        "y_intercept": -1.0,
-        "error_scale": 0.5
-    }
-    test_point = PlasmaState.merge_parameters(test_params)
-
-    opt_result = minimize(
-        fun=posterior.cost,
-        x0=test_point,
-        jac=posterior.cost_gradient
-    )
-
-    num_grad = approx_fprime(
-        xk=test_point,
-        f=posterior.log_probability,
-        epsilon=1e-8,
-    )
-    analytic_grad = posterior.gradient(test_point)
-
-    assert abs(analytic_grad/num_grad - 1).max() < 1e-6
+    run_uncertainty_model(linear_uncertainty)
