ee217a689099f4e6cb2c826f2195ab9ce1a086da
C-bowman <chris.bowman.physics@gmail.com>
2025-12-26 20:09:10 +0000

    Added a prototype beta distribution prior

--- midas/priors/__init__.py
+++ midas/priors/__init__.py
@@ -2,10 +2,12 @@ from midas.state import BasePrior
 from midas.priors.gaussian import GaussianPrior
 from midas.priors.gp import GaussianProcessPrior
 from midas.priors.exponential import ExponentialPrior
+from midas.priors.beta import BetaPrior
 
 __all__ = [
     "BasePrior",
     "GaussianPrior",
     "GaussianProcessPrior",
     "ExponentialPrior",
+    "BetaPrior",
 ]
