c62d61fa34ee485fda61a72efa521611095c0acd
C-bowman <chris.bowman.physics@gmail.com>
2026-02-06 21:45:30 +0000

    Added docs for the `SoftLimitPrior`

--- midas/priors/limits.py
+++ midas/priors/limits.py
@@ -4,12 +4,40 @@ from midas.parameters import Parameters, Fields, FieldRequest
 
 
 class SoftLimitPrior(BasePrior):
+    """
+    A prior which is uniform up to a given upper-limit, and beyond which is
+    a Gaussian with a given standard deviation. This allows a 'soft' limit
+    to be imposed on a set of given field values.
+
+    Alternatively, a 2D matrix operator can also be given, and the prior will instead
+    be applied to the result of the matrix multiplication of that operator and the
+    vector of requested field-values.
+
+    :param name: \
+        The name used to identify the prior.
+
+    :param upper_limit: \
+        The upper limit beyond which a Gaussian prior is applied to the field values.
+
+    :param standard_deviation: \
+        The standard deviation of the Gaussian prior applied to each field value which is
+        above the given upper limit.
+
+    :param field_request: \
+        A ``FieldRequest`` specifying the field values to which the prior is applied.
+
+    :param operator: \
+        A linear operator (as a 2D array) which matrix-multiplies the vector of
+        requested field values. If specified, the prior will be applied to the result
+        of this matrix-multiplication instead of the requested field values.
+    """
+
     def __init__(
         self,
         name: str,
-        field_request: FieldRequest,
         upper_limit: float,
-        sigma: float,
+        standard_deviation: float,
+        field_request: FieldRequest,
         operator: ndarray = None,
     ):
         self.name = name
@@ -17,7 +45,7 @@ class SoftLimitPrior(BasePrior):
         self.fields = Fields(field_request)
         self.parameters = Parameters()
         self.limit = upper_limit
-        self.sigma = sigma
+        self.sigma = standard_deviation
         self.weight = 1.0 / self.sigma**2
         self.A = operator if operator is not None else eye(field_request.size)
         assert self.A.ndim == 2
@@ -32,4 +60,3 @@ class SoftLimitPrior(BasePrior):
         v = fields[self.field_name]
         z = self.A @ v - self.limit
         return {self.field_name: -self.weight * self.A.T @ maximum(z, 0.0)}
-
