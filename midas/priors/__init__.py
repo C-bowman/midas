b2afa815d3b65e179444388c90bd9722c99670e1
C-bowman <chris.bowman.physics@gmail.com>
2026-09-12 21:53:13 +0100

    Added a new Linear-operator Gaussian prior

--- midas/priors/__init__.py
+++ midas/priors/__init__.py
@@ -4,6 +4,7 @@ from midas.priors.gp import GaussianProcessPrior
 from midas.priors.exponential import ExponentialPrior
 from midas.priors.beta import BetaPrior
 from midas.priors.limits import SoftLimitPrior
+from midas.priors.linear import LinearGaussianPrior
 
 __all__ = [
     "BasePrior",
@@ -12,4 +13,5 @@ __all__ = [
     "ExponentialPrior",
     "BetaPrior",
     "SoftLimitPrior",
+    "LinearGaussianPrior",
 ]
